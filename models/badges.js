159e2746d986ac87d10e520a795b91d5d90b4f14
Daniela Becerra <madajiji@gmail.com>
2016-02-16 23:14:09 -0600

    start working with database and model

---
+++ models/badges.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//require the database
+var redis = require('../lib/redis');
+
+//save badges to database
+/*
+ * @param {Array} badges
+ * @param {Function} callback
+ */
+exports.save = function(badges, callback){
+
+};
