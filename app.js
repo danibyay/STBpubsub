211ba0a660ef8ac7306fb63428036d791a4e472c
Daniela Becerra <madajiji@gmail.com>
2016-02-16 22:55:58 -0600

    Add more structure, app.js and badges.js

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var badges = require('./controllers/badges');
+
+app.use(express.json());
+//returns middleware to parse incoming json data from a request
+
+//global middleware
+app.post('/', badges.save, badges.send);
+
+app.listen(8000);
+//when the app is ran, the process is now running
---
+++ controllers/badges.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var _ = require('underscore');
+var model = require('../models/badges');
+
+//send badges to model
+exports.save = function(req, res, next){
+  var badges = _.clone(req.body); //instead of referencing
+  model.save(badges, function(err){
+    if(err) {return res.json(503, {error : true });}
+    next();
+  });
+};
+
+
+//send badges to put/sub socket in model
+exports.send = function(){};
