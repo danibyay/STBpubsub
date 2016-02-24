8de48f7be145246c1c87aa9e4f6591e6c9d0b374
Daniela Becerra <madajiji@gmail.com>
2016-02-23 23:45:26 -0600

    implement get method (get badges)

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ app.use(express.json());
 //returns middleware to parse incoming json data from a request
 
 //global middleware
-app.post('/', badges.save, badges.send, function(req, res){
-  res.send('\ndone\n\n');
-});
+app.post('/', badges.save, badges.send);
+
+
+//new route
+app.get('/badges', badges.get);
 
 app.listen(8000);
 //when the app is ran, the process is now running
--- models/badges.js
+++ models/badges.js
@@ -38,3 +38,18 @@ exports.trim = function(){
    //we're not calling the callback, but maybe in the future we'll
    //want to, and for now the method is ready
  };
+
+
+/*
+ * Get badges from redis
+ * @param {Function} callback
+ */
+ exports.get = function(callback){
+   redis.lrange('badges', 0, -1, function(err, data){
+     //propagate the error back up to the controller
+     if(err) { return callback(err, null); }
+     data = data.map(JSON.parse);
+     callback(null, data.map(JSON.parse));
+     //return the data to the controller
+   }); //give me all of them
+ };
